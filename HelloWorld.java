c6c5555b38ed36d7df9451bbb7e64c144e0173a5
Radhesh-Patil <rapatil280442@gmail.con>
2024-05-30 15:39:38 +0530

    second commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,6 @@ public class HelloWorld{
 public static void main(String args[]){
 	
 	System.out.println("Hello World");
-	System.out.println("This from Branch1")
-	System.out.println("This from Branch-2")	
+	
 	}
 }
